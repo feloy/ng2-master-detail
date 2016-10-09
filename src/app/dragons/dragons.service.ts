f9d9b4adb645b4e451d3d18f099094def5342fa6
pmartin <contact@elol.fr>
2016-10-09 19:05:02 +0200

    add a dragons service

---
+++ src/app/dragons/dragons.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+
+export interface Dragon {
+  id: number;
+  name: string;
+}
+
+@Injectable()
+export class DragonsService {
+
+  private dragons: Dragon[] = [
+    { id: 1, name: 'Dragon 1' },
+    { id: 2, name: 'Dragon 2' },
+    { id: 3, name: 'Dragon 3' },
+    { id: 4, name: 'Dragon 4' }
+  ];
+
+
+  constructor() { }
+
+  dragonsList(): Dragon[] {
+    return this.dragons;
+  }
+
+  dragonsDetails(id): Dragon {
+    let list = this.dragons.filter(d => d.id === id);
+    return list.length ? list[0] : null;
+  }
+
+}
