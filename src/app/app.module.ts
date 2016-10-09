1b5720d1552861a267c78666c1cb8053ebca5ec4
pmartin <contact@elol.fr>
2016-10-09 12:15:35 +0200

    Add an Home Component, a link to /dragons and a DragonsModule to be lazy laoded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { routing } from './app.routing';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule.forRoot()
+    MaterialModule.forRoot(),
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.ts
@@ -0,0 +1,10 @@
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: '', pathMatch: 'full', component: HomeComponent },
+  { path: 'dragons', loadChildren: 'app/dragons/dragons.module#DragonsModule' }
+];
+
+export const routing = RouterModule.forRoot(appRoutes);
